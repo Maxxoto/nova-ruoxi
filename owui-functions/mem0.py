717271acc74294674ca2b14546d861c6d9943fd6
Maxxoto <ahmatdanis24@gmail.com>
2025-06-18 01:42:38 +0800

    feat: add mem0 pipelines

---
+++ owui-functions/mem0.py
@@ -0,0 +1,167 @@
+"""
+title: Long Term Memory Filter
+author:
+date: 2024-08-23
+version: 1.0
+license: MIT
+description: A filter that processes user messages and stores them as long term memory by utilizing the mem0 framework together with qdrant
+requirements: pydantic,mem0ai
+"""
+
+import os
+from typing import List, Optional
+from pydantic import BaseModel
+import json
+from mem0 import Memory
+import threading
+
+
+class Pipeline:
+    class Valves(BaseModel):
+        pipelines: List[str] = []
+        priority: int = 0
+        store_cycles: Optional[int] = (
+            10  # Number of messages from the user before the data is processed and added to the memory
+        )
+        vector_store_qdrant_name: Optional[str] = "nova"
+        vector_store_qdrant_url: Optional[str] = "host.docker.internal"
+        vector_store_qdrant_port: Optional[int] = 6333
+        API_KEY: Optional[str] = ""  # API key
+
+    def __init__(self):
+        self.type = "filter"
+        self.name = "Memory Filter"
+        self.user_messages = []
+        self.thread = None
+
+        self.mem_zero = None
+        self.valves = self.Valves(
+            **{
+                "pipelines": ["*"],  # Connect to all pipelines
+                "vector_store_qdrant_name": os.getenv(
+                    "VECTOR_STORE_QDRANT_NAME", "memories"
+                ),
+                "vector_store_qdrant_url": os.getenv(
+                    "VECTOR_STORE_QDRANT_URL", "172.17.0.1"
+                ),
+                "vector_store_qdrant_port": os.getenv("VECTOR_STORE_QDRANT_PORT", 6333),
+                "API_KEY": os.getenv("API_KEY", "your-openai-api-key"),
+                "store_cycles": os.getenv("STORE_CYCLES", 10),
+            }
+        )
+
+    async def on_startup(self):
+        print(f"on_startup:{__name__}")
+        pass
+
+    async def on_shutdown(self):
+        print(f"on_shutdown:{__name__}")
+        pass
+
+    async def on_valves_updated(self):
+        print(f"on_valves_updated:{__name__}")
+        if self.mem_zero:
+            self.mem_zero = None  # Reset the memory to force reinitialization
+        self.mem_zero = self.init_mem_zero()
+        pass
+
+    async def inlet(
+        self,
+        body: dict,
+        user: Optional[dict] = None,
+    ) -> dict:
+        print(f"pipe:{__name__}")
+
+        store_cycles = self.valves.store_cycles
+
+        try:
+            mem0_user = user.get("id")
+        except (TypeError, KeyError):
+            print("Could not retrieve user ID. Using default user.")
+            raise ValueError("User ID is not set")
+
+        if self.API_KEY == "":
+            raise ValueError("API key is not set")
+
+        if isinstance(body, str):
+            body = json.loads(body)
+
+        all_messages = body["messages"]
+        last_message = all_messages[-1]["content"]
+
+        self.user_messages.append(last_message)
+
+        if len(self.user_messages) == store_cycles:
+            message_text = ""
+            for message in self.user_messages:
+                message_text += message + " "
+
+            if self.thread and self.thread.is_alive():
+                print("Waiting for previous memory to be done")
+                self.thread.join()
+
+            self.thread = threading.Thread(
+                target=self.m.add, kwargs={"data": message_text, "user_id": mem0_user}
+            )
+
+            print("Text to be processed in to a memory:")
+            print(message_text)
+
+            self.thread.start()
+            self.user_messages.clear()
+
+        memories = self.m.search(last_message, user_id=mem0_user)
+
+        if memories:
+            fetched_memory = memories[0]["memory"]
+        else:
+            fetched_memory = ""
+
+        print("Memory added to the context:")
+        print(fetched_memory)
+
+        if fetched_memory:
+            all_messages.insert(
+                0,
+                {
+                    "role": "system",
+                    "content": "This is your inner voice talking, you remember this about the person you chatting with "
+                    + str(fetched_memory),
+                },
+            )
+
+        print("Final body to send to the LLM:")
+        print(body)
+
+        return body
+
+    def init_mem_zero(self):
+        config = {
+            "vector_store": {
+                "provider": "qdrant",
+                "config": {
+                    "collection_name": self.valves.vector_store_qdrant_name,
+                    "host": self.valves.vector_store_qdrant_url,
+                    "port": self.valves.vector_store_qdrant_port,
+                    "embedding_model_dims": 3072,  # Default dimension for text-embedding-3-large
+                },
+            },
+            "llm": {
+                "provider": "openai",
+                "config": {
+                    "model": "gpt-4o-mini",
+                    "temperature": 0.2,
+                    "max_tokens": 2000,
+                    "api_key": self.valves.API_KEY,
+                },
+            },
+            "embedder": {
+                "provider": "openai",
+                "config": {
+                    "model": "text-embedding-3-large",
+                    "api_key": self.valves.API_KEY,
+                },
+            },
+        }
+
+        return Memory.from_config(config)
